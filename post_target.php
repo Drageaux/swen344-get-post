b67b0e9d7b91a1b9d56d13d463d0a5243e9c796e
David Thong Nguyen <thnguyen2808@gmail.com>
2018-02-09 17:44:04 -0500

    Initial commit

---
+++ post_target.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+              integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+  <p>First Name: <?php echo $_POST['fname']; ?></p>
+  <p>Last Name: <?php echo $_POST['lname']; ?></p>
+  <p>Age: <?php echo $_POST['age']; ?></p>
+  <p>State: <?php echo $_POST['state']; ?></p>
+  <p>Gender: <?php echo $_POST['gender']; ?></p>
+
+  <p>GET - info NOT appearing in URL header</p>
+</div>
+</body>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+</html>
