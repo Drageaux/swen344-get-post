916ae457c36ccf63d57fc3374683ced8a3460448
David Thong Nguyen <thnguyen2808@gmail.com>
2018-02-09 18:05:59 -0500

    Added links to other pages

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<div class="jumbotron">
+    <h2>SWEN-344 Get Post Exercise</h2>
+    <p><a class="btn btn-primary btn-lg" role="button" a href="get.html">Get page</a></p>
+    <p><a class="btn btn-primary btn-lg" role="button" href="post.html">Post page</a></p>
+</div>
+</div>
+</body
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+</html>
